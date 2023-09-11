b554171e3993a8208f4afb3865cb3a7952d31cdb
adryanni123 <103949938+adryanni123@users.noreply.github.com>
2023-09-11 11:39:31 -0300

    h

---
+++ css/imagens/main.js
@@ -0,0 +1,28 @@
+function tocaSom (idElementoAudio)}
+   document.querySelector(idElementoAudio).play();
+  }
+
+  const listaDeTeclas=document.querySelectorAll(´.tecla`)
+    
+    //para
+    for (let contador)=0; contador<listaDeTeclas.length;contador++){
+
+   const tecla = listaDeTeclas[contador];
+   const instrumento = tecla.classLista[1];
+   const idAudio = `#som_${instrumento}`;//template string
+
+
+     tecla.onclick = function(){  
+        tocaSom(idAudio);
+
+     }
+
+    }
+
+
+
+
+
+
+
+
